cd0999451a2f759b950cef2402c05cdc5df8e05a
jayson <eulric_loire@yahoo.com>
2025-04-02 07:23:45 +0000

    restart 2.1

--- emze/emze.py
+++ emze/emze.py
@@ -1,29 +1,19 @@
 import reflex as rx
-from pages.landing_page import index
-from pages.customer_dash import admin_dashboard
 
 
-
-from rxconfig import config
-
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-style={    
-    "height": "100vh",
-    "width": "100vw",
-    "font" : "Arial"
-    }
+from .pages.landing_page import index
+from .pages.dashboard import  dash_board
 
 
 
 
-app = rx.App(style=style)
-app.add_page(index, image="favicon.ico" )
-# app.add_page(admin_dashboard)
+app = rx.App(
+    theme=rx.theme(
+        appearance="dark", has_background=True, radius="large", accent_color="grass"
+    ),
+)
 
 
 
+app.add_page(index, route="/")
+app.add_page(dash_board, route="/dash")
